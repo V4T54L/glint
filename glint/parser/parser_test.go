32d8773c8d55ed29fe22c622a0d7fab93b395b92
Vatsal <vatsalpatel26003@gmail.com>
2025-11-07 02:51:57 +0530

    add parsing errors aggregator.

--- glint/parser/parser_test.go
+++ glint/parser/parser_test.go
@@ -18,6 +18,9 @@ let foobar = 838383;
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
+
+	checkParserErrors(t, p)
+
 	if len(program.Statements) != 3 {
 		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
 			len(program.Statements))
@@ -55,4 +58,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
